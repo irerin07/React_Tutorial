994b7d895118150834c3add8cf346d015e64c0d1
irerin07 <counterchord89@gmail.com>
2019-11-18 19:50:18 +0900

    middleware

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -75,19 +75,3 @@ class SelectColors extends Component {
 // };
 
 export default SelectColors;
-
-import React from "react";
-import { Route } from "react-router-dom";
-import About from "./About";
-import Home from "./Home";
-
-const App = () => {
-  return (
-    <div>
-      <Route path="/" component={Home} exact={true} />
-      <Route path={["/about", "/info"]} component={About} />
-    </div>
-  );
-};
-
-export default App;
